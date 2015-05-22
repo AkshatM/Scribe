82f7754c62248a9fbfbab8ee4c512621fa3da06f
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-05-21 19:13:18 -0700

    Ran very simple Javascript as Chrome extension

--- client/listener.js
+++ client/listener.js
@@ -1,3 +1,4 @@
-var URLregexp = new RegExp("/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/");
+//var URLregexp = new RegExp("/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/");
+alert("You have come to Papa!");
 
 
