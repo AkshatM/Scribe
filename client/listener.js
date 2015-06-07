6f95174d895c689a249c2251030a6574ef1cebc7
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-06-06 17:55:05 -0700

    Complete!

--- client/listener.js
+++ client/listener.js
@@ -7,7 +7,13 @@ document.onkeyup = function (event) {
       for (i = 0; i < x.length; i++){
           // iterate through x[i] and match to urlRegex
 	  x[i].innerHTML = x[i].innerText.replace(urlRegex, function(url){
-	      return '<a href="' + url + '">' + HTTPResponse(url) + '</a>';
+	      var final_value = HTTPResponse(url);
+	      if (final_value[1].indexOf("image") != -1){
+	         // embed image
+		 return '<img src = "data:'+final_value[1]+';base64,'+final_value[0]+'"/>';
+	         } else {
+	            return '<a href="' + url + '">' + final_value[0] + '</a>';
+	         }
 	      });
 	  }
    };
@@ -20,6 +26,5 @@ function HTTPResponse(url){
    xmlHttp.open("GET",native_url+url, false); //currently synchronous; must change to asynchronous
    xmlHttp.setRequestHeader("X-Mashape-Key", mashape_api_key);
    xmlHttp.send();
-   console.log(xmlHttp.status);
-   return xmlHttp.responseText;
+   return [xmlHttp.responseText, xmlHttp.getResponseHeader("content-type")];
 }
