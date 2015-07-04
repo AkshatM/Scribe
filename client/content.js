c3dee7d14e1b4e4bb1b7f4a782c220bef50cd509
Akshat Mahajan <akshatm.bkk@gmail.com>
2015-07-04 16:10:41 +0530

    Heading towards first release.

--- client/content.js
+++ client/content.js
@@ -5,3 +5,5 @@ s.src = chrome.extension.getURL('listener.js');
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('Autolinker.min.js');
 (document.head || document.documentElement).appendChild(s);
+
+chrome.extension.sendRequest("show_page_action");
